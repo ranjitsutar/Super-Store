3b6bf5244bf681a4f5be9244eef7c54f99bc0d63
Ranjit Sutar <ranjitsutar>
2024-04-01 23:23:12 +0530

    initial setup project

---
+++ src/main/java/com/superstore/superstore/SuperstoreApplication.java
@@ -0,0 +1,13 @@
+package com.superstore.superstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superstore/superstore/SuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superstore.superstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
